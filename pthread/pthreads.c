7e1a81fcfe7455908192b63ec3351b4b567f1467
Morten <mortenkretschmer@yahoo.de>
2023-02-02 21:06:35 +0100

    add nprocs

--- pthread/pthreads.c
+++ pthread/pthreads.c
@@ -3,12 +3,15 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+#include <sys/sysinfo.h>
+
 
 #define MAX_THREADS 100000
 #define PTHREAD_STACK_MIN 1*1024*1024*1024
 int i;
 
 void run(void) {
+  //std::cout << "Thread ID: " << pthread_self() << std::endl;
   sleep(60 * 60);
 }
 
@@ -16,21 +19,22 @@ int main(int argc, char *argv[]) {
   int rc = 0;
   pthread_t thread[MAX_THREADS];
   pthread_attr_t thread_attr;
-
+  int nprocs = get_nprocs();
   pthread_attr_init(&thread_attr);
   pthread_attr_setstacksize(&thread_attr, PTHREAD_STACK_MIN);
 
   printf("Creating threads ...\n");
   for (i = 0; i < MAX_THREADS && rc == 0; i++) {
     rc = pthread_create(&(thread[i]), &thread_attr, (void *) &run, NULL);
+    nprocs = get_nprocs();
     if (rc == 0) {
       pthread_detach(thread[i]);
       //printf("Thread %i created with PID %lu\n", i + 1, (unsigned long) pthread_self());
       if ((i + 1) % 100 == 0)
-        printf("%i threads with PID %lu so far ...\n", i + 1, (unsigned long) pthread_self());
+        printf("%i threads with nprocs: %d so far ...\n", i + 1, nprocs);
     } else {
-      printf("Failed with return code %i creating thread %i PID %lu (%s).\n",
-         rc, i + 1, (unsigned long) pthread_self(), strerror(rc));
+      printf("Failed with return code %i creating thread %i nprocs: %d (%s).\n",
+         rc, i + 1,nprocs , strerror(rc));
 
       // can we allocate memory?
       char *block = NULL;
